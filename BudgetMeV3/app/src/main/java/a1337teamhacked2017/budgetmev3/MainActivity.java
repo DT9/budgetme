a4888a669233ca30edba337289f1d49929233c95
MarcusBoay <marcusboay@hotmail.com>
2017-01-29 01:04:34 -0700

    asda

--- BudgetMeV3/app/src/main/java/a1337teamhacked2017/budgetmev3/MainActivity.java
+++ BudgetMeV3/app/src/main/java/a1337teamhacked2017/budgetmev3/MainActivity.java
@@ -5,8 +5,11 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 
+<<<<<<< HEAD
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+=======
+>>>>>>> parent of 09ecf85... Got Firebase to work
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -16,11 +19,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(myToolbar);
 
-        // Write a message to the database
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("users");
-
-        myRef.setValue("GG NO RE");
     }
 
     @Override
