cace966d8b77d39c1622dbea0f25cc44db599fa4
MarcusBoay <marcusboay@hotmail.com>
2017-01-28 18:50:44 -0700

    Made buttons in action bar

--- BudgetMeV3/app/src/main/java/a1337teamhacked2017/budgetmev3/MainActivity.java
+++ BudgetMeV3/app/src/main/java/a1337teamhacked2017/budgetmev3/MainActivity.java
@@ -2,6 +2,8 @@ package a1337teamhacked2017.budgetmev3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,6 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_activities, menu);
+        return true;
     }
 }
